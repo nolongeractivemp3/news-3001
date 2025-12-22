1f744cb044d7d0f5f064b75a3a742b0a6491eebe
avg <handyjason053@gmail.com>
2025-12-23 00:42:18 +0100

    it works :)

--- ui/index.php
+++ ui/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
@@ -12,13 +13,13 @@
             color: white;
         }
     </style>
-    <main class="container mx-auto">
+    <main class="p-4">
         <?php
         $response = file_get_contents("http://backend:5000");
         $news = json_decode($response, true);
-        echo "<div class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>";
+        echo "<div class='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>";
         foreach ($news as $item) {
-            echo "<div class='card bg-base-100 shadow-sm'>";
+            echo "<div class='card bg-base-100 shadow-sm min-w-0'>";
             echo "<div class='card-body'>";
             echo "<h2 class='card-title'>" .
                 htmlspecialchars($item["source"]) .
