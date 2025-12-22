5542c50e1351023de9a8175e4bef37535235b292
avg <handyjason053@gmail.com>
2025-12-23 00:25:38 +0100

    refactored the ui 3

--- ui/index.php
+++ ui/index.php
@@ -1,33 +1,41 @@
+<!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="css/pico.min.css" />
-    <link rel="stylesheet" href="css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <html data-theme="dark"></html>
 </head>
 <body>
-    <main class="container">
+    <style>
+        body {
+            background-color: 333446;
+            color: white;
+        }
+    </style>
+    <main class="container mx-auto">
         <?php
         $response = file_get_contents("http://backend:5000");
         $news = json_decode($response, true);
-
+        echo "<div class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>";
         foreach ($news as $item) {
-            echo "<div class='card w-96 bg-base-100 card-xs shadow-sm' style='background-color: black; color: white;'>";
+            echo "<div class='card bg-base-100 shadow-sm'>";
             echo "<div class='card-body'>";
-            echo "<header>";
-            echo "<strong>" . htmlspecialchars($item["source"]) . "</strong>";
-            echo "</header>";
-
-            echo "<h5>" . htmlspecialchars($item["title"]) . "</h5>";
+            echo "<h2 class='card-title'>" .
+                htmlspecialchars($item["source"]) .
+                "</h2>";
+            echo "<p>" . htmlspecialchars($item["title"]) . "</p>";
             echo "<p>" . htmlspecialchars($item["description"]) . "</p>";
-
-            echo "<footer>";
+            echo "<div class='card-actions justify-end'>";
+            echo '<button class="btn btn-primary">';
             echo '<a target="_blank" href="' .
                 htmlspecialchars($item["link"]) .
-                '" role="button" class="outline">Read More</a>';
-            echo "</footer>";
+                '">Read More</a>';
+            echo "</button>";
+            echo "</div>";
             echo "</div>";
             echo "</div>";
         }
+        echo "</div>";
         ?>
     </main>
 
