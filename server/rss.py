29285a8779df5b39eb74b5dd0f0eabd7a0e26743
JayPoch <handyjason053@gmail.com>
2025-12-29 01:36:25 +0100

    changed rss to add checks for date

--- server/rss.py
+++ server/rss.py
@@ -1,3 +1,6 @@
+import datetime
+from sqlite3.dbapi2 import Time
+
 import feedparser
 from bs4 import BeautifulSoup as bs
 
@@ -6,23 +9,37 @@ from objects import News
 url = "https://www.google.de/alerts/feeds/17016683277093386427/7293694784079144231"
 
 
-# def get_rss_feed() -> list[News]:
-def get_rss_feed():
+def check_date(date: str):
+    print(date)
+    x = date.split("T")
+    dates = x[0]
+    times = x[1][: len(x[1]) - 1]
+    y = datetime.datetime.strptime(f"{dates} {times}", "%Y-%m-%d %H:%M:%S")
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    print(y)
+    print(yesterday)
+    if y > yesterday:
+        return True
+    return False
+
+
+def get_rss_feed() -> list[News]:
     feed = feedparser.parse(url)
     news = []
     rawnews = feed.items().mapping["entries"]
     for i in range(len(rawnews)):
-        print()
-        news.append(
-            News(
-                "RSS feed",
-                bs(rawnews[i].title, "html.parser").get_text(),
-                bs(rawnews[i].content[0]["value"], "html.parser").get_text(),
-                rawnews[i].link,
+        if check_date(rawnews[i].published):
+            news.append(
+                News(
+                    "RSS feed",
+                    bs(rawnews[i].title, "html.parser").get_text(),
+                    bs(rawnews[i].content[0]["value"], "html.parser").get_text(),
+                    rawnews[i].link,
+                )
             )
-        )
+        print(rawnews[i])
     return news
 
 
 if __name__ == "__main__":
-    get_rss_feed()
+    rss_feed = get_rss_feed()
