f016ec696615e7f0f82bb07f28d61676db62e27f
avg <handyjason053@gmail.com>
2026-01-02 19:41:06 +0100

    finally fixed the ui for realsys

--- ui/components/report.php
+++ ui/components/report.php
@@ -1,29 +1,34 @@
 <style>
-#report_modal {
-  padding: 0;
-  border: none;
-  background: transparent;
+h1 {
+ font-size: 1rem;
+ font-weight: bold;
+ margin-bottom: 0.2rem;
 }
 
-#report_modal::backdrop {
-  background: rgba(0, 0, 0, 0.6);
-  backdrop-filter: blur(4px);
+h2 {
+ font-size: 0.875rem;
+ font-weight: bold;
+ margin-bottom: 0.1rem;
 }
 
+h3 {
+ font-size: 0.75rem;
+ font-weight: bold;
+ margin-bottom: 0.05rem;
+}
 
 </style>
-<dialog id="report_modal" class="modal">
-    <div class="modal-box w-full md:w-11/12 max-w-3xl border border-primary/20 bg-neutral ">
-        <form method="dialog">
-            <button class="btn btn-sm btn-circle btn-ghost absolute right-4 top-4 text-primary">✕</button>
-        </form>
-        <article id="ReportContent" class="prose prose-invert prose-sm max-w-none
-                        prose-h1:text-xl prose-h1:mb-2
-                        prose-h2:text-lg prose-h2:mb-1
-                        prose-h3:text-base prose-h3:mb-1
-                        break-words"><?php echo $reportstr; ?></article>
-    </div>
-    <form method="dialog" class="modal-backdrop bg-black/60 backdrop-blur-sm">
-        <button>close</button>
+
+<dialog id="report_modal" class="modal block overflow-y-auto bg-black/60 backdrop-blur-sm">
+  <div class="relative w-[95%] max-w-3xl my-8 mx-auto border border-primary/20 bg-neutral shadow-2xl p-6 rounded-2xl">
+
+    <form method="dialog">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-4 top-2 text-primary">✕</button>
     </form>
+
+    <article id="ReportContent" class="prose prose-invert prose-sm max-w-none text-left break-words">
+       <?php echo $reportstr; ?>
+    </article>
+
+  </div>
 </dialog>
